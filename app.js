45b49f2bd3b2ec0f1490cf38bb239ecd45ef3188
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2020-02-09 10:18:22 -0800

    Adding UI tweaks
    Adding more UI tweaks

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 //Item Controller -- IIFE
 const ItemCtrl = (function () {
+    'use strict';
     // Item constructor
     const Item = function (id, name, calories) {
         this.id = id;
@@ -11,21 +12,23 @@ const ItemCtrl = (function () {
     };
 
     const addItem = (name, calories) => {
-        data.items.push(new Item(data.items.length, name,calories));
+        const newItem = new Item(data.items.length, name, calories);
+        data.items.push(newItem);
+        return newItem;
     };
 
     // DataStructure -- State
     const data = {
         items: [
-            {
-                id: 0, name: 'Steak Dinner', calories: 1200
-            },
-            {
-                id: 1, name: 'Cookie', calories: 200
-            },
-            {
-                id: 2, name: 'Eggs', calories: 100
-            },
+            // {
+            //     id: 0, name: 'Steak Dinner', calories: 1200
+            // },
+            // {
+            //     id: 1, name: 'Cookie', calories: 200
+            // },
+            // {
+            //     id: 2, name: 'Eggs', calories: 100
+            // },
         ],
         currentItem: null,
         totalCalories: 0
@@ -40,7 +43,7 @@ const ItemCtrl = (function () {
 
 //UI Controller
 const UICtrl = (function () {
-
+    'use strict';
     const UISelectors = {
         itemList: '#item-list',
         addBtn: '.add-btn',
@@ -60,8 +63,25 @@ const UICtrl = (function () {
         document.querySelector(UISelectors.errorBlock).textContent = ' ';
     };
 
+    const clearInputs = () => {
+        document.querySelector(UISelectors.itemNameInput).value = '';
+        document.querySelector(UISelectors.itemCaloriesInput).value = '';
+    };
+
     const addError = (message) => {
         document.querySelector(UISelectors.errorBlock).textContent = message;
+    };
+
+    const hideList = () => {
+        document.querySelector(UISelectors.itemList).style.display = 'none';
+    };
+
+    const addListItem = (newItem) => {
+        document.querySelector(UISelectors.itemList).innerHTML += `<li class="collection-item" id="item-${newItem.id}">
+                            <strong>${newItem.name}: </strong> <em>${newItem.calories} Calories</em>
+                            <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+                        </li>`;
+        document.querySelector(UISelectors.itemList).style.display = 'block';
     };
 
     const populateItems = (items) => {
@@ -83,13 +103,16 @@ const UICtrl = (function () {
         getUISelectors: () => UISelectors,
         getItemInput,
         clearErrors,
-        addError
+        addError,
+        addListItem,
+        clearInputs,
+        hideList
     };
 })();
 
 //App Controller
 const App = (function (ItemCtrl, UICtrl) {
-
+    'use strict';
     const loadEventListeners = () => {
         const UISelectors = UICtrl.getUISelectors();
         document.querySelector(UISelectors.addBtn).addEventListener('click', (event) => {
@@ -104,16 +127,19 @@ const App = (function (ItemCtrl, UICtrl) {
                     UICtrl.addError('Enter valid Calorie Count');
                 } else {
                     UICtrl.clearErrors();
+                    // Add item to the data structure
                     const newItem = ItemCtrl.addItem(itemToAdd.name, validCalories);
+                    // Add item to the UI, We can just retrigger populate UI or we can also write a separate function for adding one single item
+                    // UICtrl.populateItemList(ItemCtrl.getItems());
+                    UICtrl.addListItem(newItem);
+                    // Clear UI elements
+                    UICtrl.clearInputs();
                     console.log(ItemCtrl.logData());
                 }
 
             } else {
                UICtrl.addError('Enter valid Item Name and Calorie count');
             }
-
-
-
         });
     };
 
@@ -122,6 +148,11 @@ const App = (function (ItemCtrl, UICtrl) {
             console.log('Initializing App');
             // Fetch items from the data structure -- could be an external api as well
             const items = ItemCtrl.getItems();
+
+            // Check if items are present
+            if (!items || items.length === 0) {
+                UICtrl.hideList();
+            }
 
             // Populate list with items
             UICtrl.populateItemList(items);
